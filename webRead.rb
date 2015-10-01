a840f844dec8678e01b37fcdfb1448a5b784ba37
Takuya Mizunaka <is0089he@gmail.com>
2015-10-02 07:35:46 +0900

    htmlソースの作成、正規表現ソースの変更

--- webRead.rb
+++ webRead.rb
@@ -7,28 +7,23 @@ filename = "anchor_tag.html"
 textfile = "anchor_tag.txt"
 
 # urlの読み込み、書き出し
-File.open(filename , :encoding => Encoding::UTF_8) do |f|
+File.open(filename,"w") do |f|
     text = open(url).read
     f.write text
 end
 
 # 書き出しファイルの読み込み
-html = File.read(filename)
+html = File.read(filename,:encoding => Encoding::UTF_8)
 
 # 読み込みファイルからの抽出、書き出し
-File.open(textfile ,:encoding => Encoding::UTF_8) do |f|
-    headinfo = true
+File.open(textfile,"w") do |f|
     html.each_line do |line|
-        if headinfo && /<meta/ !~ line
+        if /<a\shref/ !~ line
             # && 左辺がtrueだった場合に、右辺を評価する
             # !~ 正規表現のメソッド。=!の否定。マッチが失敗したらtrueを返す
             next
-        else
-            headinfo = false
         end
         break if /<\/head>/ =~ line
-        # =~ 正規表現のメソッド。正規表現と文字列をマッチさせる。
-        # </head>で処理を終了
         f.write line
     end
 end
