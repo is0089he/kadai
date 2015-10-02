ee7672f7fc17f15d227798ba8d6a64feeacb391c
Takuya Mizunaka <is0089he@gmail.com>
2015-10-03 07:02:50 +0900

    アンカータグのみ抽出

--- webRead.rb
+++ webRead.rb
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 require "open-uri"
 require 'kconv'
+require 'csv'
 
 url = "http://www.weblio-inc.jp/wordpress/company/outline/"
 filename = "anchor_tag.html"
-textfile = "anchor_tag.txt"
+textfile = "anchor_tag.csv"
 
 # urlの読み込み、書き出し
 File.open(filename,"w") do |f|
@@ -18,12 +19,13 @@ html = File.read(filename,:encoding => Encoding::UTF_8)
 # 読み込みファイルからの抽出、書き出し
 File.open(textfile,"w") do |f|
     html.each_line do |line|
-        if /<a\shref/ !~ line
-            # && 左辺がtrueだった場合に、右辺を評価する
-            # !~ 正規表現のメソッド。=!の否定。マッチが失敗したらtrueを返す
-            next
+
+        # アンカータグの正規表現
+        # .* : なんでも良い文字の連続
+        if /<a\shref.*a>/ =~ line
+            f.write $&
+            f.write "\n"
+
         end
-        break if /<\/head>/ =~ line
-        f.write line
     end
 end
